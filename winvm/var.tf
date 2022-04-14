bb9a49af3d28cd58c5bef2f05591e8c95447fd51
gaurav <gauravkumar9130@gmail.com>
2022-04-14 11:33:13 +0530

    module created

---
+++ winvm/var.tf
@@ -0,0 +1,17 @@
+variable "rg_name" {}
+
+variable "vnet_name" {
+    default = "vnet1"
+}
+
+variable "subnet_name" {
+    default = "subnet1"
+}
+
+variable "nic_name" {
+    default = "nic1"
+}
+
+variable "vm_name" {
+    default = "vm1"
+}
